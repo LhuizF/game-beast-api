729d978afba4da72d5b890a7daf845da56c0788a
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-08-21 20:10:25 -0300

    test: add email validator adapter

---
+++ test/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,27 @@
+import { EmailValidatorAdapter } from '../../src/utils/email-validator';
+import validator from 'validator';
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true;
+  }
+}));
+
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter();
+};
+
+describe('EmailValidator Adapter', () => {
+  test('Should return false if validator return false', () => {
+    const sut = makeSut();
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
+    const isValid = sut.isValid('invalidEmail');
+    expect(isValid).toBe(false);
+  });
+
+  test('Should return true if validator return true', () => {
+    const sut = makeSut();
+    const isValid = sut.isValid('validEmail');
+    expect(isValid).toBe(true);
+  });
+});
