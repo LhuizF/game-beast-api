d7c6477569507457b108bc336f862a95faa574e7
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-08-28 20:54:24 -0300

    refactor: move errors to a file

--- src/presentation/erros/index.ts
+++ src/presentation/erros/index.ts
@@ -1,3 +1,20 @@
-export * from './missing-param-error';
-export * from './invalid-param-error';
-export * from './server-error';
+export class InvalidParamError extends Error {
+  constructor(public paramName: string) {
+    super(`Invalid param: ${paramName}`);
+    this.name = 'InvalidParamError';
+  }
+}
+
+export class MissingParamError extends Error {
+  constructor(public paramName: string) {
+    super(`Missing param: ${paramName}`);
+    this.name = 'MissingParamError';
+  }
+}
+
+export class ServerError extends Error {
+  constructor() {
+    super('Internal server error');
+    this.name = 'ServerError';
+  }
+}
