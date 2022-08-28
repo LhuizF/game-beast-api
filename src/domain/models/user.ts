ea32b57cf77d505d50ad99283f4957c428de82c3
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-08-28 13:50:11 -0300

    refactor: update User model

--- src/domain/models/user.ts
+++ src/domain/models/user.ts
@@ -1,10 +1,10 @@
 export interface UserModel {
   id: string;
   name: string;
-  id_guild?: string;
-  id_discord?: string;
-  email?: string;
-  password?: string;
-  avatar?: string;
+  id_guild?: string | null;
+  id_discord?: string | null;
+  email?: string | null;
+  password?: string | null;
+  avatar: string | null;
   created_at: Date;
 }
