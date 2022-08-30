099340af5c0e5e14211734cbedc248e9f0518b77
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-08-30 00:42:15 -0300

    feat: add CreateGuild Controller

---
+++ src/presentation/controllers/guild/createGuild.ts
@@ -0,0 +1,36 @@
+import { AddGuild } from '../../../domain/usecases/add-guild';
+import { MissingParamError } from '../../erros';
+import { badRequest, ok, serverError } from '../../helpers';
+import { Controller, HttpRequest, HttpResponse } from '../../protocols';
+
+class CreateGuildController implements Controller {
+  constructor(private readonly addGuild: AddGuild) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const fields = ['id', 'name', 'channel'];
+      for (const field of fields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
+      const { id, name, channel } = httpRequest.body;
+
+      const icon =
+        httpRequest.body.icon ||
+        'https://cdn-icons-png.flaticon.com/512/5968/5968756.png';
+
+      const guild = await this.addGuild.add({
+        id,
+        name,
+        icon,
+        channel
+      });
+      return ok(guild);
+    } catch (error) {
+      return serverError();
+    }
+  }
+}
+
+export default CreateGuildController;
