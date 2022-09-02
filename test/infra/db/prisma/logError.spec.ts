2fbb01277dfe79d10de59c727329b527b464c41e
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-09-01 22:09:39 -0300

    test: add LogErrorRepository

---
+++ test/infra/db/prisma/logError.spec.ts
@@ -0,0 +1,30 @@
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
+import { PrismaClient } from '@prisma/client';
+import { LogErrorPrismaRepository } from '../../../../src/infra/db/prisma/logError';
+import { prisma } from '../../../../src/infra/db/prisma';
+
+jest.useFakeTimers().setSystemTime(new Date());
+
+const makeSut = () => {
+  const mock = mockDeep<PrismaClient>();
+  const prismaMock: DeepMockProxy<PrismaClient> =
+    mock as unknown as DeepMockProxy<PrismaClient>;
+  const sut = new LogErrorPrismaRepository(prisma);
+
+  return {
+    sut,
+    prismaMock
+  };
+};
+
+describe('LogErrorPrismaRepository', () => {
+  test('Save log db', async () => {
+    const { sut, prismaMock } = makeSut();
+
+    await sut.logError('any_error');
+
+    const count = await prisma.logError.count();
+
+    expect(count).toBe(1);
+  });
+});
