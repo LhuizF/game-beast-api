d01887b7acbe2f2c9916ddf8051208cde2079f63
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-09-07 19:47:20 -0300

    refactor: prisma helper

--- src/infra/db/prisma/utils/helper.ts
+++ src/infra/db/prisma/utils/helper.ts
@@ -1,12 +1,13 @@
-import { HelperDb } from '../../../data/protocols/helperDb';
+import { HelperDb } from '../../../../data/protocols/helperDb';
+import { UserModel } from '../../../../domain/models/user';
 import { prisma } from './client';
 
 export class PrismaHelper implements HelperDb {
   async verifyGuild(id: number) {
     await prisma.guild.findUniqueOrThrow({ where: { id } });
   }
-  async verifyUser(id: string): Promise<void> {
-    await prisma.user.findUniqueOrThrow({ where: { id } });
+  async verifyUser(id: string): Promise<UserModel> {
+    return await prisma.user.findUniqueOrThrow({ where: { id } });
   }
   async verifyBeast(id: number): Promise<void> {
     await prisma.beast.findUniqueOrThrow({ where: { id } });
