429f196028c7c776c158fd8a2a72534f7d3ef723
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-09-11 19:17:22 -0300

    refactor: remove GameTimeAdapter for makeCreateBet

--- src/main/factories/createBet.ts
+++ src/main/factories/createBet.ts
@@ -5,14 +5,12 @@ import { LogControllerDecorator } from '../decorator/logsError';
 import { LogErrorPrismaRepository } from '../../infra/db/prisma/logError';
 import { PlaceBetDb } from '../../data/usecases/placeBetDb';
 import { PrismaHelper } from '../../infra/db/prisma/utils/helper';
-import { GameTimeAdapter } from '../../utils/game-time';
 import { UpdatePointsPrisma } from '../../infra/db/prisma/utils/updatePoints';
 
 export const makeCreateBet = (): Controller => {
   const saveBetRepository = new SaveBetPrismaRepository(prisma);
-  const gameTime = new GameTimeAdapter();
   const updatePoints = new UpdatePointsPrisma();
-  const placeBet = new PlaceBetDb(saveBetRepository, gameTime, updatePoints);
+  const placeBet = new PlaceBetDb(saveBetRepository, updatePoints);
   const helperDb = new PrismaHelper();
   const createGuildController = new CreateBetController(placeBet, helperDb);
   const logErrorPrismaRepository = new LogErrorPrismaRepository(prisma);
