e4feb4bf27748ea9304606168b23b479239e5bdd
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-09-11 22:12:38 -0300

    refactor: WinBeastPrisma for update bets, user and game

--- src/infra/db/prisma/utils/winBeast.ts
+++ src/infra/db/prisma/utils/winBeast.ts
@@ -1,18 +1,67 @@
 import { WinBeast } from '../../../../data/protocols/winBeast';
+import { BetsResult, UserWin } from '../../../../presentation/protocols/play-result';
 import { prisma } from './client';
 
 export class WinBeastPrisma implements WinBeast {
-  async addWin(id: number, time: number): Promise<void> {
+  async addWin(idGame: number, idBeast: number): Promise<BetsResult> {
+    const multiplier = 3;
+
+    // atualizando beast sorteado
     await prisma.beast.update({
-      where: { id },
+      where: { id: idBeast },
       data: { times_win: { increment: 1 } }
     });
 
-    const betsWin = await prisma.bet.updateMany({
-      where: { AND: [{ game_time: time }, { status: 'pending' }] },
-      data: { status: 'win' }
+    // atualizando resultado do game
+    await prisma.game.update({
+      where: { id: idGame },
+      data: { result: idBeast }
     });
 
-    console.log(betsWin);
+    // pegando bets vencedoras
+    const betsWin = await prisma.bet.findMany({
+      where: { AND: [{ id_game: idGame }, { status: 'pending' }, { id_beast: idBeast }] },
+      include: {
+        User: true
+      }
+    });
+
+    // atualizando pontos de cada usuário com bets vencedoras
+    const winners: UserWin[] = await Promise.all(
+      betsWin.map(async (bet) => {
+        const additionalPoints = bet.points * multiplier;
+
+        await prisma.user.update({
+          where: { id: bet.id_user },
+          data: { points: { increment: additionalPoints } }
+        });
+
+        await prisma.bet.update({
+          where: { id: bet.id },
+          data: { status: 'win' }
+        });
+
+        return {
+          id: bet.id_user,
+          name: bet.User.name,
+          avatar: bet.User.avatar,
+          pointsBet: bet.points,
+          pointsReceived: additionalPoints
+        };
+      })
+    );
+
+    const betsLose = await prisma.bet.updateMany({
+      where: {
+        AND: [{ id_game: idGame }, { status: 'pending' }, { id_beast: { not: idBeast } }]
+      },
+      data: { status: 'lose' }
+    });
+
+    return {
+      totalBets: betsLose.count + betsWin.length,
+      winners,
+      losers: betsLose.count
+    };
   }
 }
