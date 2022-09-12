037fcd9a9146d356b11540b4e26969674e1e51ec
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-09-11 22:14:33 -0300

    test: PlayResultService for get current game

--- test/presentation/controllers/service/playResult.spec.ts
+++ test/presentation/controllers/service/playResult.spec.ts
@@ -1,11 +1,26 @@
 import { WinBeast } from '../../../../src/data/protocols/winBeast';
+import { BetsResult } from '../../../../src/presentation/protocols/play-result';
 import PlayResultService from '../../../../src/presentation/service/playResult';
 import { HelperDbStub } from '../../../helper';
 
 const makeSut = () => {
   class WinBeastStub implements WinBeast {
-    async addWin(id: number): Promise<void> {
-      await new Promise((resolve) => resolve(null));
+    async addWin(idGame: number, idBeast: number): Promise<BetsResult> {
+      return await new Promise((resolve) =>
+        resolve({
+          totalBets: 2,
+          winners: [
+            {
+              id: 'any_id',
+              name: 'any_name',
+              avatar: 'any_avatar',
+              pointsBet: 10,
+              pointsReceived: 30
+            }
+          ],
+          losers: 1
+        })
+      );
     }
   }
 
@@ -24,12 +39,67 @@ describe('PlayResult Service', () => {
     expect(getAllBeastSpy).toHaveBeenCalled();
   });
 
-  test('should call addWin', async () => {
-    const { sut, winBeastStub } = makeSut();
-    const addWinSpy = jest.spyOn(winBeastStub, 'addWin');
+  test('should call getCurrentGameId', async () => {
+    const { sut, helperDbStub } = makeSut();
+    const getCurrentGameIdSpy = jest.spyOn(helperDbStub, 'getCurrentGameId');
+    await sut.play();
+
+    expect(getCurrentGameIdSpy).toHaveBeenCalled();
+  });
+
+  test('should call beastSelected current values', async () => {
+    const { sut, helperDbStub } = makeSut();
+    const beasts = await helperDbStub.getAllBeast();
+    const beastSelectedSpy = jest.spyOn(sut, 'beastSelected');
 
     await sut.play();
 
-    expect(addWinSpy).toHaveBeenCalled();
+    expect(beastSelectedSpy).toHaveBeenCalledWith(beasts);
+  });
+
+  test('should call beastSelected current values', async () => {
+    const { sut, helperDbStub } = makeSut();
+    const beasts = await helperDbStub.getAllBeast();
+    const beastSelectedSpy = jest.spyOn(sut, 'beastSelected');
+
+    await sut.play();
+
+    expect(beastSelectedSpy).toHaveBeenCalledWith(beasts);
+  });
+
+  test('should call addWin current values', async () => {
+    const { sut, winBeastStub, helperDbStub } = makeSut();
+    const addWinSpy = jest.spyOn(winBeastStub, 'addWin');
+    jest
+      .spyOn(helperDbStub, 'getCurrentGameId')
+      .mockReturnValueOnce(new Promise((resolve) => resolve(1)));
+
+    jest
+      .spyOn(sut, 'beastSelected')
+      .mockReturnValueOnce({ id: 1, name: 'any', times_win: 0 });
+
+    await sut.play();
+
+    expect(addWinSpy).toHaveBeenCalledWith(1, 1);
+  });
+
+  test('return isSuccess false if beasts.length to equal 0', async () => {
+    const { sut, helperDbStub } = makeSut();
+    jest
+      .spyOn(helperDbStub, 'getAllBeast')
+      .mockReturnValueOnce(new Promise((resolve) => resolve([])));
+
+    const result = await sut.play();
+
+    expect(result).toEqual({
+      isSuccess: false,
+      data: {
+        date: new Date(),
+        body: {
+          beasts: [],
+          game: 1
+        }
+      }
+    });
   });
 });
