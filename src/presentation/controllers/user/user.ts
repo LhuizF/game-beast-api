9cbfdab77e06a41a4be37c0de5d2de0d174967b8
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-09-15 22:32:30 -0300

    feat: add UserController

---
+++ src/presentation/controllers/user/user.ts
@@ -0,0 +1,31 @@
+import { HelperDb } from '../../../data/protocols/helperDb';
+import { MissingParamError, InvalidParamError } from '../../erros';
+import { badRequest, ok, serverError } from '../../helpers';
+import { Controller, HttpRequest, HttpResponse } from '../../protocols';
+
+class UserController implements Controller {
+  constructor(private readonly helperDb: HelperDb) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { id_user } = httpRequest.params;
+
+      if (!id_user) {
+        return badRequest(new MissingParamError('id_user'));
+      }
+
+      const user = await this.helperDb.getUser(id_user);
+
+      if (!user) {
+        return badRequest(new InvalidParamError('id_user'));
+      }
+
+      return ok(user);
+    } catch (error) {
+      console.log(error);
+      return serverError(error);
+    }
+  }
+}
+
+export default UserController;
