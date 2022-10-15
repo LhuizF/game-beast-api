c4b7ff8bc3395064de9eab2374cd2a9295124026
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-10-15 16:16:27 -0300

    feat: add RankController

---
+++ src/presentation/controllers/rank/rank.ts
@@ -0,0 +1,20 @@
+import { Controller, HttpRequest, HttpResponse } from '../../protocols';
+import { HelperDb } from '../../../data/protocols/helperDb';
+import { ok, serverError } from '../../helpers';
+
+export class RankController implements Controller {
+  constructor(private readonly helperDb: HelperDb) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const serverId = httpRequest.query?.server;
+
+      const rank = await this.helperDb.getRank(serverId);
+
+      return ok(rank);
+    } catch (error) {
+      console.log(error);
+      return serverError(error);
+    }
+  }
+}
