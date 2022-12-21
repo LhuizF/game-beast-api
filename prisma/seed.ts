eb2b90a67b75472286f9786307ccca8df8bfdc16
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-12-20 23:54:01 -0300

    feat: add build script

---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import beast from '../test/mocks/beasts.json';
+import { prisma } from '../src/infra/db/prisma/utils/client';
+import { GameTimeAdapter } from '../src/utils/game-time';
+import { SaveGamePrismaRepository } from '../src/infra/db/prisma/saveGame';
+
+const run = async () => {
+  try {
+    await prisma.beast.createMany({ data: beast });
+
+    const gameTime = new GameTimeAdapter();
+    const time = gameTime.getNext();
+
+    const saveGame = new SaveGamePrismaRepository(prisma);
+    await saveGame.save(time);
+
+    console.log('Done');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+run();
--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,37 @@
 import fs from 'fs';
+import path from 'path';
 import { Express, Router } from 'express';
 
 const useRoutes = (app: Express) => {
+  const folder = path.join(__dirname, '../', './routes');
+  const prefix = '/v1';
   const router = Router();
-  app.use('/v1', router);
-  fs.readdirSync('./src/main/routes').map(async (file) => {
+
+  app.use(prefix, router);
+  fs.readdirSync(folder).map(async (file, i) => {
     (await import(`../routes/${file}`)).default(router);
-    console.log('router ->', file);
+    // console.log('router ->', file);
+
+    if (process.env.NODE_ENV !== 'DEV') return;
+
+    if (i === fs.readdirSync(folder).length - 1) {
+      console.log('\nAll routes');
+
+      router.stack.forEach((r) => {
+        const methods = Object.keys(r.route.methods);
+
+        methods.forEach((m) => {
+          const method = (() => {
+            if (m.length <= 3) {
+              return m + '  ->';
+            }
+            return m + ' ->';
+          })();
+
+          console.log(method.toUpperCase(), prefix + r.route.path);
+        });
+      });
+    }
   });
 };
 
