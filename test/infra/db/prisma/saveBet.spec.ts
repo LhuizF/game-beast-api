d359e03a7e86f60460246ff33db93f4375f527e9
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-12-22 23:06:54 -0300

    fix: test main file

--- test/infra/db/prisma/saveBet.spec.ts
+++ test/infra/db/prisma/saveBet.spec.ts
@@ -1,4 +1,4 @@
-import { SaveBetPrismaRepository } from '../../../../src/infra/db/prisma/saveBet';
+import { SaveBetPrismaRepository } from '../../../../src/infra/db/prisma/mainSaveBet';
 import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 import { PrismaClient } from '@prisma/client';
 import { Bet } from '../../../../src/data/protocols/saveBetRepository';
