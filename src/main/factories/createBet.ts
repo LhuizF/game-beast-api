d6f5c64d7c4f4ee0b0ea3169fcef88268eec6b73
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-12-23 20:33:34 -0300

    fix: import

--- src/main/factories/createBet.ts
+++ src/main/factories/createBet.ts
@@ -1,14 +1,13 @@
 import CreateBetController from '../../presentation/controllers/bet/createBet';
 import { prisma, SaveBetPrismaRepository } from '../../infra/db/prisma';
 import { Controller } from '../../presentation/protocols';
-import { LogControllerDecorator } from '../decorator/logsError';
+import { LogControllerDecorator } from '../decorator';
 import { LogErrorPrismaRepository } from '../../infra/db/prisma/logError';
 import { PlaceBetDb } from '../../data/usecases/placeBetDb';
 import { PrismaHelper } from '../../infra/db/prisma/utils/helper';
 import { UpdatePointsPrisma } from '../../infra/db/prisma/utils/updatePoints';
 
 export const makeCreateBet = (): Controller => {
-  console.log('makeCreateBet ->', __dirname);
   const saveBetRepository = new SaveBetPrismaRepository(prisma);
   const updatePoints = new UpdatePointsPrisma();
   const placeBet = new PlaceBetDb(saveBetRepository, updatePoints);
