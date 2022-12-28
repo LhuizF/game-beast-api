b026cae01b139d7801e3f445e25273fe76079e58
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-12-28 15:46:38 -0300

    hotfix: correct time in gameTime

--- src/utils/game-time.ts
+++ src/utils/game-time.ts
@@ -8,9 +8,9 @@ export class GameTimeAdapter implements GameTime {
     const currentSeconds = Number(hour) * 3600 + Number(min) * 60 + Number(seconds);
 
     const time = (() => {
-      if (currentSeconds <= 36000) return 2;
-      if (currentSeconds <= 54000) return 3;
-      return 1;
+      if (currentSeconds >= 72000) return 1;
+      if (currentSeconds >= 54000) return 3;
+      return 2;
     })();
 
     console.log({
@@ -28,9 +28,3 @@ export const time = {
   2: 'Tarde',
   3: 'Noite'
 };
-
-/**
- * 1 Manha 10hs 36000 next => 2
- * 2 Tarde 15hs 54000 next => 3
- * 3 Noite 20hs 72000 next => 1
- */
